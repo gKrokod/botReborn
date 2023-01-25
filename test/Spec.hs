0b4b1ae57593fa0f5d2826716d3f51ce5811db1a
gKrokod <t9013054362@gmail.com>
2023-01-25 20:50:09 +0300

    do test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,35 +1,65 @@
 import Test.Hspec
 import Test.QuickCheck
+import Types
+import Control.Monad.Identity
+import qualified Handlers.Logger
+import qualified Handlers.Base
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
+  describe "Base logic" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      func ([]) `shouldBe` Nothing
-  describe "NEW***************" $ do
+  describe "Bot logic" $ do
     it "returns the first element of a list" $ do
       head [23 ..] `shouldBe` (23 :: Int)
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
+  describe "Client logic" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
 
-    it "throws an exception if used with an empty list" $ do
-      func ([]) `shouldBe` Nothing
+  describe "Dispatcher logic" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
 
--- spec :: Spec
--- spec = 
---   describe "try" $ do
---     it "first try" $
---       func [] `shouldBe` Nothing
---     it "second" $
---       func [1] `shouldBe` (Just 1)
---
-func :: [Int] -> Maybe Int
-func [] = Nothing
-func (x:xs) = Just x
+  describe "Logger logic" $ do
+    it "No logic, no test" $ do
+      Debug `shouldBe` Debug
+
+testConfig :: Config 
+testConfig = Config
+	     {  cRepeatCount = 3 :: RepeatCount
+	     ,  cTextMenuHelp = "Help menu:" :: T.Text 
+	     ,  cTextMenuRepeat = "Repeat menu:" :: T.Text
+	     ,  cLvlLog = Debug :: Log
+	     } 
+
+logHandle :: Config -> Handlers.Logger.Handle Identity
+logHandle cfg = Handlers.Logger.Handle
+	       { Handlers.Logger.levelLogger = cLvlLog cfg-- Fatal
+	       , Handlers.Logger.writeLog = \text -> pure () 
+	       }
+baseHandle :: Config -> Handlers.Base.Handle Identity
+baseHandle cfg = Handlers.Base.Handle
+		 {  Handlers.Base.defaultRepeatCount = cRepeatCount cfg
+		 ,  Handlers.Base.readStackMessage = pure (Nothing, Nothing)
+		 ,  Handlers.Base.saveMessage = Base.saveMessage stackMessage
+		 ,  Handlers.Base.eraseMessage = Base.eraseMessage stackMessage
+		 ,  Handlers.Base.findUser = Base.findUser base
+		 ,  Handlers.Base.updateUser = Base.updateUser base
+		 ,  Handlers.Base.logger = logHandle
+		 }
+-- data Handle m = Handle 
+--   {  defaultRepeatCount :: RepeatCount
+--   ,  readStackMessage :: m (Maybe Message, Maybe LastMessage)
+--   ,  saveMessage :: Message -> m ()
+--   ,  eraseMessage :: Message -> m ()
+--   ,  findUser :: User -> m (Maybe RepeatCount)
+--   ,  updateUser :: User -> RepeatCount -> m ()
+--   ,  logger :: HL.Handle m  
+--   }
