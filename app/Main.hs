afb71d97ae0d33e7bcd70f32babd874c2dc7393b
gKrokod <t9013054362@gmail.com>
2023-01-25 18:39:08 +0300

    add some test examples

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ import qualified Handlers.Client
 import qualified ClientConsole
 import qualified Handlers.Dispatcher
 import qualified Dispatcher
+import qualified Handlers.Logger
+import qualified Logger
 import qualified Config (loadConfig)
 import System.IO
 -- import qualified Data.Text as T
@@ -31,6 +33,11 @@ main = do
   base <- Base.newBaseUser
   cfg <- Config.loadConfig
 
+  let logHandle = Handlers.Logger.Handle
+                  { Handlers.Logger.levelLogger = cLvlLog cfg -- Fatal
+		  , Handlers.Logger.writeLog = Logger.writeLog
+		  }
+
   let baseHandle = Handlers.Base.Handle
                    {  Handlers.Base.defaultRepeatCount = cRepeatCount cfg
 		   ,  Handlers.Base.readStackMessage = Base.readStackMessage stackMessage
@@ -38,25 +45,31 @@ main = do
 		   ,  Handlers.Base.eraseMessage = Base.eraseMessage stackMessage
 		   ,  Handlers.Base.findUser = Base.findUser base
 		   ,  Handlers.Base.updateUser = Base.updateUser base
+		   ,  Handlers.Base.logger = logHandle
 		   }
   
   let clientHandle = Handlers.Client.Handle
                      { Handlers.Client.fetch = ClientConsole.fetch
 		     , Handlers.Client.carryAway = ClientConsole.carryAway
+		     , Handlers.Client.logger = logHandle
 	             }
 
   let botHandle = Handlers.Bot.Handle 
-               { Handlers.Bot.getMessage = undefined --Handlers.Dispatcher.getMessage dispatcherHandle 1
-	       , Handlers.Bot.sendMessage = ClientConsole.carryAway
-	       , Handlers.Bot.base = baseHandle
-	       , Handlers.Bot.helpMessage = cTextMenuHelp cfg
-	       , Handlers.Bot.repeatMessage = cTextMenuRepeat cfg}
+		  { Handlers.Bot.getMessage = undefined --Handlers.Dispatcher.getMessage dispatcherHandle 1
+		  , Handlers.Bot.sendMessage = ClientConsole.carryAway
+		  , Handlers.Bot.base = baseHandle
+		  , Handlers.Bot.helpMessage = cTextMenuHelp cfg
+		  , Handlers.Bot.repeatMessage = cTextMenuRepeat cfg
+		  , Handlers.Bot.logger = logHandle
+		  }
 
   let handle = Handlers.Dispatcher.Handle
-                   { Handlers.Dispatcher.client = clientHandle
-		   , Handlers.Dispatcher.bot = botHandle
-		   , Handlers.Dispatcher.forkForUser = Dispatcher.forkForUser
-		   }
+	       { Handlers.Dispatcher.client = clientHandle
+	       , Handlers.Dispatcher.bot = botHandle
+	       , Handlers.Dispatcher.forkForUser = Dispatcher.forkForUser
+	       , Handlers.Dispatcher.logger = logHandle
+	       }
+
   forkIO $ forever (do Handlers.Dispatcher.watcherForNewMessage handle; threadDelay (100))
   print "Watcher is running"
   print "Dispatcher is running"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      func ([]) `shouldBe` Nothing
+  describe "NEW***************" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      func ([]) `shouldBe` Nothing
+
+-- spec :: Spec
+-- spec = 
+--   describe "try" $ do
+--     it "first try" $
+--       func [] `shouldBe` Nothing
+--     it "second" $
+--       func [1] `shouldBe` (Just 1)
+--
+func :: [Int] -> Maybe Int
+func [] = Nothing
+func (x:xs) = Just x
