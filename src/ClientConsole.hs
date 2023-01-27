cd1fba25ae9bdf8d37d25df3c57233d380031ca1
gKrokod <t9013054362@gmail.com>
2023-01-27 20:05:50 +0300

    add some test and some cosmetic fix

--- src/ClientConsole.hs
+++ src/ClientConsole.hs
@@ -9,21 +9,20 @@ import qualified Data.Time.Clock.System as Time
 
 fetch :: Maybe LastMessage -> IO (Maybe Message)
 fetch lm = do
-  -- Опционально добавь сюда заддержку threadDelay
   m <- getLine
   time <- Time.getSystemTime
-  let msg = Message { mID = fromIntegral $ Time.systemSeconds time, mUser = 1}
+  let msg = Message {mID = fromIntegral $ Time.systemSeconds time, mUser = 1}
   case lm of
-    Nothing -> makeMessage m msg
+    Nothing -> pure $ makeMessage m msg
     Just lm -> if mID lm == mID msg
                then pure Nothing
-	       else makeMessage m msg
+	       else pure $ makeMessage m msg
 
-makeMessage :: String -> Message -> IO (Maybe Message)
+makeMessage :: String -> Message -> Maybe Message
 makeMessage t msg = case t of
-  "/help"   -> pure $ Just $ msg {mData = Command "/help"}
-  "/repeat" -> pure $ Just $ msg {mData = Command "/repeat"}
-  otherwise -> pure $ Just $ msg {mData = Msg $ T.pack t}
+  "/help"   -> Just $ msg {mData = Command "/help"}
+  "/repeat" -> Just $ msg {mData = Command "/repeat"}
+  otherwise -> Just $ msg {mData = Msg $ T.pack t}
 
 
 carryAway :: Message -> IO ()
@@ -31,7 +30,5 @@ carryAway msg = case mData msg of
                     Query i      -> print i
 		    Msg t        -> do 
 		                      TIO.putStrLn t
-				      -- TIO.putStrLn (T.pack $ show $ mID msg) -- тогда надо АЙД задаватьлоЖц
-				      -- лол
 		    KeyboardMenu -> TIO.putStrLn ("Type a new repeat count [1..5]: ")
 		    otherwise    -> pure ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,13 @@
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import Test.QuickCheck
+import Text.Read
 import Types
 import Control.Monad.Identity
+import Data.Char (isDigit)
 import qualified Handlers.Logger
 import qualified Handlers.Base
+import qualified Handlers.Bot
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
@@ -12,20 +16,39 @@ import qualified Data.Text.IO as TIO
 main :: IO ()
 main = hspec $ do
   describe "Base logic" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+    let bHandle = baseHandle testConfig
+    it "returns the default repeat count from Config for new users" $
+      property $ \user -> runIdentity (Handlers.Base.giveRepeatCountFromBase bHandle user) == cRepeatCount testConfig
 
-  describe "Bot logic" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+    it "returns default repeat counts from Config for the new user" $
+      property $ \count -> do
+        let bHandle = baseHandle (testConfig {cRepeatCount = count})
+        runIdentity (Handlers.Base.giveRepeatCountFromBase bHandle (1 :: User)) == count
+
+  describe "Bot logic" $ modifyMaxSuccess (const 1000) $ do
+    context "User can change repeat counts only in range [1..5]" $ do
+      it "Input: Int" $ do
+	property $ \answer -> do
+	  let msg = Message {mData = Query answer, mID = 1, mUser = 2}
+	  Handlers.Bot.isCorrectRepeatCount msg == (1 <= answer && answer <= 5)
+
+      it "Input: Text" $ do
+	property $ \answer -> do
+	  let numberMessage = maybe 0 id (readMaybe answer :: Maybe Int)
+	  let msg = Message {mData = Msg (T.pack answer), mID = 1, mUser = 2}	 
+	  Handlers.Bot.isCorrectRepeatCount msg == (1 <= numberMessage && (numberMessage <= 5))
+    
+    context "Correct answer on:" $ do
+      it "/help" $ do
+        Debug `shouldBe` Debug
 
   describe "Client logic" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
-
-  describe "Dispatcher logic" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+    it "No logic, no test" $ do
+      Debug `shouldBe` Debug
+  --
+  -- describe "Dispatcher logic" $ do
+  --   it "returns the first element of a list" $ do
+  --     head [23 ..] `shouldBe` (23 :: Int)
 
   describe "Logger logic" $ do
     it "No logic, no test" $ do
@@ -44,22 +67,33 @@ logHandle cfg = Handlers.Logger.Handle
 	       { Handlers.Logger.levelLogger = cLvlLog cfg-- Fatal
 	       , Handlers.Logger.writeLog = \text -> pure () 
 	       }
+
 baseHandle :: Config -> Handlers.Base.Handle Identity
 baseHandle cfg = Handlers.Base.Handle
 		 {  Handlers.Base.defaultRepeatCount = cRepeatCount cfg
 		 ,  Handlers.Base.readStackMessage = pure (Nothing, Nothing)
-		 ,  Handlers.Base.saveMessage = Base.saveMessage stackMessage
-		 ,  Handlers.Base.eraseMessage = Base.eraseMessage stackMessage
-		 ,  Handlers.Base.findUser = Base.findUser base
-		 ,  Handlers.Base.updateUser = Base.updateUser base
-		 ,  Handlers.Base.logger = logHandle
+		 ,  Handlers.Base.saveMessage = \_ -> pure ()
+		 ,  Handlers.Base.eraseMessage = \_ -> pure ()
+		 ,  Handlers.Base.findUser = \user -> pure (Just $ cRepeatCount cfg)
+		 ,  Handlers.Base.updateUser = \user count -> pure ()
+		 ,  Handlers.Base.logger = logHandle cfg
 		 }
--- data Handle m = Handle 
---   {  defaultRepeatCount :: RepeatCount
---   ,  readStackMessage :: m (Maybe Message, Maybe LastMessage)
---   ,  saveMessage :: Message -> m ()
---   ,  eraseMessage :: Message -> m ()
---   ,  findUser :: User -> m (Maybe RepeatCount)
---   ,  updateUser :: User -> RepeatCount -> m ()
+
+botHandle :: Config -> Handlers.Bot.Handle Identity
+botHandle cfg = Handlers.Bot.Handle 
+		{ Handlers.Bot.getMessage = pure (Message {mData = Msg ("Test message"), mID = 1, mUser = 2})
+		, Handlers.Bot.sendMessage = \text -> pure ()
+		, Handlers.Bot.base = baseHandle cfg
+		, Handlers.Bot.helpMessage = cTextMenuHelp cfg
+		, Handlers.Bot.repeatMessage = cTextMenuRepeat cfg
+		, Handlers.Bot.logger = logHandle cfg
+		}
+
+-- data Handle m = Handle
+--   {  getMessage :: m (Message)
+--   ,  sendMessage :: Message -> m ()
+--   ,  base :: Handlers.Base.Handle m
+--   ,  helpMessage :: T.Text
+--   ,  repeatMessage :: T.Text
 --   ,  logger :: HL.Handle m  
 --   }
